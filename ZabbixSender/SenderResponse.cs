2f452c475b17acdf0720821cfd39415ecd998973
Alexander Andreev <andreevlex.as@gmail.com>
2018-03-31 13:27:12 +0300

    simple impl zabbix protocol

---
+++ ZabbixSender/SenderResponse.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Zabbix
+{
+    public class SenderResponse
+    {
+        [JsonProperty(PropertyName = "response")]
+        public string Response { get; set; }
+
+        [JsonProperty(PropertyName = "")]
+        public string Info { get; set; }
+        public bool Success { get { return Response == "success"; }}
+    }
+}
