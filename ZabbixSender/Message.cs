03a0085ced3d3f5d7beb6c10e67c120cd33cfcf3
Alexander Andreev <andreevlex.as@gmail.com>
2018-04-04 21:42:41 +0300

    added  support send more 1 value

--- ZabbixSender/Message.cs
+++ ZabbixSender/Message.cs
@@ -1,19 +1,25 @@
 ﻿using System;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace Zabbix
 {
-	public class Message
+	sealed public class Message
 	{
 		[JsonProperty(PropertyName = "request")]
 		private readonly string request = "sender data"; 
 
 		[JsonProperty(PropertyName = "data")] 
-		private readonly SendValue[] data;
+		private readonly List<SendValue> data;
 
 		public Message(SendValue sendvalue)
 		{
-			data = new [] { sendvalue };
+			data = new List<SendValue> { sendvalue };
+		}
+		public Message(IEnumerable<SendValue> sendvalues)
+		{
+			data = new List<SendValue>();
+			data.AddRange(sendvalues);
 		}
 	}
 }
--- cli_sender/Program.cs
+++ cli_sender/Program.cs
@@ -7,10 +7,26 @@ namespace cli_sender
 	{
 		public static void Main(string[] args)
 		{
-			var msg = new Message(new SendValue { Host = "host1", Key = "key1", Value = "Тест кириллицы" });
-			var sender = new Sender("192.168.0.3");
+			if(args.Length < 1){
+				Environment.Exit(1);
+			}
+
+			string host = args[0];
+
+			var val1 = new SendValue { Host = "host1", Key = "key1", Value = "Тест кириллицы" };
+			var val2 = new SendValue { Host = "host1", Key = "key1", Value = "Значение 2"};
+
+			// Example1
+			var msg = new Message(val1);
+			var sender = new Sender(host);
 			var response = sender.Send(msg);
 			Console.WriteLine(response.Success);
+
+			var arr = new SendValue[] { val1, val2};
+			var msg2 = new Message(arr);
+			var sender2 = new Sender(host);
+			var response2 = sender2.Send(msg2);
+			System.Console.WriteLine(response2.Success);
 		}
 	}
 }
