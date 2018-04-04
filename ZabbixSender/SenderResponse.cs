0dfff915cb5b7138e5fab11f4dd1a3cc970b74a0
Alexander Andreev <andreevlex.as@gmail.com>
2018-04-04 22:28:49 +0300

    added parse Info

--- ZabbixSender/SenderResponse.cs
+++ ZabbixSender/SenderResponse.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Text.RegularExpressions;
 
 namespace Zabbix
 {
@@ -11,5 +12,48 @@ namespace Zabbix
 		[JsonProperty(PropertyName = "info")]
 		public string Info { get; set; }
 		public bool Success { get { return Response == "success"; }}
+
+		public double Processed { 
+			get {
+				return Convert.ToInt32(
+					GetValueFromInfo("processed")
+				);
+			 }
+		}
+
+		public double Failed { 
+			get {
+				return Convert.ToInt32(
+					GetValueFromInfo("failed")
+				);
+			 }
+		}
+
+		public double Total { 
+			get {
+				return Convert.ToInt32(
+					GetValueFromInfo("total")
+				);
+			 }
+		}
+
+		public double SecondsSpent { 
+			get {
+				return Convert.ToDouble(
+					GetValueFromInfo("seconds_spent")
+				);
+			 }
+		}
+
+		private string pattern = 
+		@"processed: (?<processed>\d+); failed: (?<failed>\d+); total: (?<total>\d+); seconds spent: (?<seconds_spent>\d\.\d+)";
+
+		private string GetValueFromInfo(string name)
+		{
+			var reg = new Regex( pattern );
+			var match = reg.Match(Info);
+			
+			return match.Groups[name].Value;
+		}
 	}
 }
--- cli_sender/Program.cs
+++ cli_sender/Program.cs
@@ -20,13 +20,15 @@ namespace cli_sender
 			var msg = new Message(val1);
 			var sender = new Sender(host);
 			var response = sender.Send(msg);
-			Console.WriteLine(response.Success);
+			Console.WriteLine("example1: {0}", response.Success);
+			System.Console.WriteLine("processed: {0}, seconds: {1, 3:f6}", response.Processed, response.SecondsSpent);
 
 			var arr = new SendValue[] { val1, val2};
 			var msg2 = new Message(arr);
 			var sender2 = new Sender(host);
 			var response2 = sender2.Send(msg2);
-			System.Console.WriteLine(response2.Success);
+			Console.WriteLine("example2: {0}", response2.Success);
+			System.Console.WriteLine("processed: {0}, seconds: {1, 3:f6}", response2.Processed, response2.SecondsSpent);
 		}
 	}
 }
